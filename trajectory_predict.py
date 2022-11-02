cd77255e016feb4ef14429a91946eb3ae7f9a101
pranavvk21 <pranavvkumar21@gmail.com>
2022-11-02 18:28:23 +0000

    initial commit

---
+++ trajectory_predict.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+import cv2 as cv
+import numpy as np
+import utils
+import os
+cap = cv.VideoCapture(0)
+size= 480
+cap.set(cv.CAP_PROP_FRAME_WIDTH, 800)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 600)
+kernel = np.ones((3,3),np.uint8)
+dilate_kernel = np.ones((20,20),np.uint8)
+prev = [float("inf"),float("inf")]
+mov=1000
+count=0
+start_img=0
+stop_img=0
+cou=[]
+split_size=5
+frame_size = 256
+buff = np.empty((0,split_size,2))
+def dist(prev,curr):
+    return ((prev[0]-curr[0])**2+(prev[1]-curr[1])**2)/2
+
+class Buffer:
+    def __init__(self):
+        pass
+for i in range(10000):
+    # Take each frame
+    flag=0
+    _, frame = cap.read()
+    frame = utils.resize(frame,frame_size)
+    print(frame.shape)
+    frame = utils.g_blur(frame,5,2)
+    mask = utils.detect_ball(frame)
+
+    res = cv.bitwise_and(frame,frame, mask= mask)
+    mask = cv.dilate(mask,dilate_kernel,iterations = 4)
+    res = cv.dilate(res,dilate_kernel,iterations = 4)
+    contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    if len(contours)>0:
+        cnt = contours[0]
+        M = cv.moments(cnt)
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+
+        cv.line(frame,(cx-10,cy),(cx+10,cy),(255,0,0),5)
+        cv.line(frame,(cx,cy-10),(cx,cy+10),(255,0,0),5)
+        curr = [cx,cy]
+        if prev[0]!=float("inf"):
+            cv.line(frame,(cx,cy),(prev[0],prev[1]),(0,0,255),5)
+        dis = dist(prev,curr)
+        #print(dis)
+        prev=curr
+        prev_mov = mov
+        mov = 0 if dis<150 else 1
+        if mov==1 and prev_mov==0:
+            count+=1
+            start_img = frame
+
+        if mov ==0 and prev_mov==1:
+            for pos in range(len(cou)-1):
+                cv.line(frame,(cou[pos][0],cou[pos][1]),(cou[pos+1][0],cou[pos+1][1]),(0,0,255),5)
+                cv.circle(frame,(cou[pos][0],cou[pos][1]),10,(0,255,0),5)
+            stop_img = frame
+            main_list = cou.copy()
+            flag=1
+
+
+            cou=[]
+        if mov:
+            cou.append((curr))
+
+    cv.imshow('frame',frame)
+
+
+    k = cv.waitKey(5) & 0xFF
+    if k == 27:
+        break
+    if flag and len(main_list)>split_size :
+        print(len(main_list))
+        a = input("save ?: ")
+        if a=="y":
+            sublist = utils.list_split(main_list,split_size)
+            sample = np.array(sublist)
+            print(sample.shape)
+            buff = np.concatenate([buff,sample])
+            print(buff.shape)
+        if a=="s":
+            l = len(os.listdir("./dataset_arrays/"))+1
+            np.save("./dataset_arrays/array_"+str(l),buff)
+            print("saved")
+cv.destroyAllWindows()
---
+++ utils.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import cv2 as cv
+import numpy as np
+
+
+
+kernel = np.ones((3,3),np.uint8)
+dilate_kernel = np.ones((20,20),np.uint8)
+
+
+def resize(frame,size):
+    max_shape = min(frame.shape[0],frame.shape[1])//2
+    cent_x = frame.shape[1]//2
+    cent_y = frame.shape[0]//2
+    frame = frame[cent_y-max_shape:cent_y+max_shape,cent_x-max_shape:cent_x+max_shape,:]
+    frame = cv.resize(frame,(size,size), interpolation = cv.INTER_AREA)
+    return frame
+def g_blur(img,k_size,iterations):
+    for i in range(iterations):
+        frame = cv.GaussianBlur(img,(k_size,k_size),0)
+    return frame
+def detect_ball(frame):
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    col = np.uint8([[[0,255,142 ]]])
+    col = cv.cvtColor(col, cv.COLOR_BGR2HSV)[0,0]
+    lower = np.array([col[0]-20,120,100])
+    upper = np.array([col[0]+10,255,255])
+
+    mask = cv.inRange(hsv, lower, upper)
+    mask = cv.GaussianBlur(mask,(5,5),0)
+
+    mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel,iterations=3)
+    return mask
+
+def list_split(cou,length):
+    if len(cou)<length:
+        return []
+    elif len(cou)==length:
+        return cou
+    else:
+        sublist = []
+        for i in range(0,len(cou)-length):
+            sub = cou[i:i+length]
+            sublist.append(sub)
+        return sublist
